a8c7f55251d4a54857165dcb41e636d3bd286c8d
ferenc-a <140697579+ferenc-a@users.noreply.github.com>
2025-05-15 08:32:25 +0200

    Update kameleo_cloudflare_turnstile.py to Kameleo 4

--- 3-bypass-cloudflare-turnstile-with-Kameleo/kameleo_cloudflare_turnstile.py
+++ 3-bypass-cloudflare-turnstile-with-Kameleo/kameleo_cloudflare_turnstile.py
@@ -1,37 +1,32 @@
 from kameleo.local_api_client import KameleoLocalApiClient
-from kameleo.local_api_client.builder_for_create_profile import BuilderForCreateProfile
+from kameleo.local_api_client.models import CreateProfileRequest
 from playwright.sync_api import sync_playwright
 import time
 import os
 import json
 
-
 # This is the port Kameleo.CLI is listening on. Default value is 5050, but can be overridden in appsettings.json file
 kameleo_port = os.getenv('KAMELEO_PORT', '5050')
 
 client = KameleoLocalApiClient(
-    endpoint=f'http://localhost:{kameleo_port}',
-    retry_total=0
+    endpoint=f'http://localhost:{kameleo_port}'
 )
 
-# Search Chrome Base Profiles
-base_profiles = client.search_base_profiles(
+# Search Chrome fingerprints
+fingerprints = client.fingerprint.search_fingerprints(
     device_type='desktop',
-    os_family='windows',
-    browser_product='chrome',
+    browser_product='chrome'
 )
 
 # Create a new profile with recommended settings
-# Choose one of the Base Profiles
-create_profile_request = BuilderForCreateProfile \
-    .for_base_profile(base_profiles[0].id) \
-    .set_name('Kameleo bypass Cloudflare Turnstile') \
-    .set_recommended_defaults() \
-    .build()
-profile = client.create_profile(body=create_profile_request)
+# Choose one of the fingerprints
+create_profile_request = CreateProfileRequest(
+    fingerprint_id=fingerprints[0].id,
+    name='Kameleo bypass Cloudflare Turnstile')
+profile = client.profile.create_profile(create_profile_request)
 
 # Start the Kameleo profile and connect with Playwright through CDP
-client.start_profile(profile.id)
+client.profile.start_profile(profile.id)
 browser_ws_endpoint = f'ws://localhost:{kameleo_port}/playwright/{profile.id}'
 with sync_playwright() as playwright:
     browser = playwright.chromium.connect_over_cdp(endpoint_url=browser_ws_endpoint)
@@ -43,33 +38,36 @@ with sync_playwright() as playwright:
     with open("user_agent.txt", "w") as file:
         file.write(user_agent)
 
-    # Use any Playwright command to drive the browser
-    # and enjoy full protection from bot detection products
+    # Use any Playwright command to drive the browser and enjoy full protection from bot detection products
     page.goto('https://www.indeed.com/cmp/Burger-King/reviews')
 
-# Wait for 5 seconds
+# Wait for 15 seconds
 time.sleep(15)
 
 # Stop the browser by stopping the Kameleo profile
-client.stop_profile(profile.id)
+client.profile.stop_profile(profile.id)
 
-# Export cookies using
-exported_cookies = client.list_cookies(profile.id)
+# Export cookies using the Kameleo API
+try:
+    exported_cookies = client.cookie.list_cookies(profile.id)
 
-# Filter the cf_clearance cookie
-cf_clearance_cookie = next(
-    (cookie for cookie in exported_cookies if cookie.name == 'cf_clearance'),
-    None
-)
+    # Filter the cf_clearance cookie
+    cf_clearance_cookie = next(
+        (cookie for cookie in exported_cookies if cookie.name == 'cf_clearance'),
+        None
+    )
 
-# Save the cookie to a JSON file if found
-if cf_clearance_cookie:
-    output_data = [{
-        'domain': cf_clearance_cookie.domain,
-        'name': cf_clearance_cookie.name,
-        'value': cf_clearance_cookie.value
-    }]
-    with open('cf_clearance_cookie.json', 'w') as f:
-        json.dump(output_data, f, indent=4)
-
-print("Cookies exported successfully!")
+    # Save the cookie to a JSON file if found
+    if cf_clearance_cookie:
+        output_data = [{
+            'domain': cf_clearance_cookie.domain,
+            'name': cf_clearance_cookie.name,
+            'value': cf_clearance_cookie.value
+        }]
+        with open('cf_clearance_cookie.json', 'w') as f:
+            json.dump(output_data, f, indent=4)
+        print("Cookies exported successfully!")
+    else:
+        print("cf_clearance cookie not found")
+except Exception as e:
+    print(f"Error accessing cookies: {e}")
